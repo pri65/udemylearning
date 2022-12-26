44a36436dc08a74ac8092433a9819425ed19fbaa
pri65 <priyankasoniii65@gmail.com>
2022-12-26 15:04:22 +0530

    Leap year

--- day6/leapYear.js
+++ day6/leapYear.js
@@ -1,7 +1,15 @@
 function isLeap(year) {
-    if(year%4 == 0 || year%100 == 0 || year%400 == 0) {
-       console.log("yes it is a leap year")
-    } else {
-        console.log("No it's not a leap year")
-    }
+   if(year % 4 == 0) {
+        if(year % 100 == 0) {
+             if(year % 400 == 0) {
+                 return "Is a leap year";
+             } else {
+                return "Not a leap year";
+             }
+        } else {
+            return "Not a leap year";
+        }
+   } else {
+    return "Not a leap year";
+   }
 }
